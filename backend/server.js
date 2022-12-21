52a675192c75c66de709bf9ceca1a84f678e6cee
dewzye2 <prankbooh1@gmail.com>
2022-12-21 15:38:10 -0300

    New routes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cors({
-  origin: ["http://localhost:3000", "https://mern-task-dewzye.onrender.com", "https://mern-task-dewzye-api.onrender.com/"]
+  origin: ["http://localhost:3000", "https://mern-task-dewzye.onrender.com", "https://mern-task-dewzye-api.onrender.com"]
 }));
 
 app.use("/api/tasks", taskRoutes)
